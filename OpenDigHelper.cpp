9f901024d66e78391d0faf3f7397d1a48d56331d
RARuser789 <142449467+Dexter8733@users.noreply.github.com>
2025-09-26 19:33:22 -0400

    Update OpenDigHelper.cpp

--- OpenDigHelper.cpp
+++ OpenDigHelper.cpp
@@ -1,56 +1,54 @@
 #define _WIN32_WINNT 0x0501
-#include <windows.h>
 #include <winsock2.h>
 #include <ws2tcpip.h>
+#include <windows.h>
 #include <iostream>
 #include <string>
 #include <conio.h>
-#include <cstdio>
+#include <cstdlib>
 
 #pragma comment(lib, "ws2_32.lib")
 
 using namespace std;
 
+// WHOIS query
 string whois_query(const string& server, const string& domain) {
     WSADATA wsaData;
-    if (WSAStartup(MAKEWORD(2,2), &wsaData) != 0) return "WSAStartup failed.\n";
+    if (WSAStartup(MAKEWORD(2,2), &wsaData) != 0)
+        return "WSAStartup failed.\n";
 
-    SOCKET sock = INVALID_SOCKET;
-    struct addrinfo hints, *res = nullptr;
-    ZeroMemory(&hints, sizeof(hints));
+    struct addrinfo hints = {0}, *result;
     hints.ai_family = AF_INET;
     hints.ai_socktype = SOCK_STREAM;
-
-    if (getaddrinfo(server.c_str(), "43", &hints, &res) != 0) {
+    if (getaddrinfo(server.c_str(), "43", &hints, &result) != 0) {
         WSACleanup();
-        return "getaddrinfo failed.\n";
+        return "DNS resolution failed.\n";
     }
 
-    sock = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
+    SOCKET sock = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
     if (sock == INVALID_SOCKET) {
-        freeaddrinfo(res);
+        freeaddrinfo(result);
         WSACleanup();
         return "Socket creation failed.\n";
     }
 
-    if (connect(sock, res->ai_addr, (int)res->ai_addrlen) != 0) {
+    if (connect(sock, result->ai_addr, (int)result->ai_addrlen) == SOCKET_ERROR) {
         closesocket(sock);
-        freeaddrinfo(res);
+        freeaddrinfo(result);
         WSACleanup();
         return "Connection failed.\n";
     }
-
-    freeaddrinfo(res);
+    freeaddrinfo(result);
 
     string query = domain + "\r\n";
     send(sock, query.c_str(), (int)query.size(), 0);
 
-    char buf[1024];
-    int bytes;
+    char buffer[1024];
     string response;
-    while ((bytes = recv(sock, buf, sizeof(buf)-1, 0)) > 0) {
-        buf[bytes] = '\0';
-        response += buf;
+    int bytes;
+    while ((bytes = recv(sock, buffer, sizeof(buffer)-1, 0)) > 0) {
+        buffer[bytes] = '\0';
+        response += buffer;
     }
 
     closesocket(sock);
@@ -58,41 +56,43 @@ string whois_query(const string& server, const string& domain) {
     return response;
 }
 
-void RunSystemCommand(const string& cmd) {
-    FILE* pipe = _popen(cmd.c_str(), "r");
-    if (!pipe) {
-        cout << "Failed to run command.\n";
-        return;
-    }
-    char buffer[128];
-    while (fgets(buffer, sizeof(buffer), pipe)) {
-        cout << buffer;
-    }
-    _pclose(pipe);
+// Wait for key press before exit
+void WaitAndExit() {
+    cout << "\nPress any key to close..." << endl;
+    _getch();
 }
 
 int main(int argc, char* argv[]) {
     if (argc < 3) {
         cout << "Usage: OpenDigHelper <action> <target> [extra]\n";
-        return 1;
+        WaitAndExit();
+        return 0;
     }
 
     string action = argv[1];
     string target = argv[2];
-    string extra = (argc > 3) ? argv[3] : "";
+    string extra  = (argc > 3) ? argv[3] : "";
 
     if (action == "ping") {
-        RunSystemCommand("ping " + target);
-    } else if (action == "dig") {
-        RunSystemCommand("nslookup -type=" + extra + " " + target);
-    } else if (action == "tracert") {
-        RunSystemCommand("tracert " + target);
-    } else if (action == "whois") {
-        string result = whois_query("whois.iana.org", target);
-        cout << result;
+        string cmd = "ping " + target;
+        system(cmd.c_str());
+    }
+    else if (action == "dig") {
+        string cmd = "nslookup -type=" + extra + " " + target;
+        system(cmd.c_str());
+    }
+    else if (action == "tracert") {
+        string cmd = "tracert " + target;
+        system(cmd.c_str());
+    }
+    else if (action == "whois") {
+        cout << "WHOIS query for " << target << ":\n\n";
+        cout << whois_query("whois.iana.org", target);
+    }
+    else {
+        cout << "Unknown action: " << action << endl;
     }
 
-    cout << "\n\nPress any key to close...";
-    _getch();
+    WaitAndExit();
     return 0;
 }
